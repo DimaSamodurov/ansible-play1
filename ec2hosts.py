bc7df8d9bb8a072894c7777a851e7e925601626f
Dmytro Samodurov <dimasamodurov@gmail.com>
2017-03-13 16:14:00 +0200

    Add options to ec2hosts inventory.

--- ec2hosts.py
+++ ec2hosts.py
@@ -1,6 +1,28 @@
 import json 
 import subprocess
 import operator
+import argparse
+
+
+def main():
+    args = parse_args()
+
+    if args.list:
+        host_groups = get_ec2_hosts()
+        print_ec2_hosts(host_groups)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Preprocess ec2 described instances.')
+    parser.add_argument('--list', action='store_true', help='list all hosts')
+    args = parser.parse_args()
+    if not (args.list):
+        raise Exception('--list argument must be specified.')
+    return args
+
+
+def aws_describe_instances():
+    return subprocess.check_output(["aws", "ec2", "describe-instances"])
 
 
 def get_ec2_hosts():
@@ -8,14 +30,21 @@ def get_ec2_hosts():
 
     def get_dns_name(instance): return instance['PublicDnsName']
 
-    instances_description = subprocess.check_output(["aws", "ec2",  "describe-instances"])
+    instances_description = aws_describe_instances()
 
-    dict = json.loads(instances_description)
+    groups = json.loads(instances_description)
 
-    instances = reduce(operator.add, map(get_instances, dict['Reservations']))
+    instances = reduce(operator.add, map(get_instances, groups['Reservations']))
 
     names = map(get_dns_name, instances)
 
     return names
 
-print get_ec2_hosts()
+
+def print_ec2_hosts(host_groups):
+    print json.dumps(host_groups, indent=2, sort_keys=True)
+
+
+if __name__ == '__main__':
+    main()
+
