246a54839e8c8e2a6ea2ece28e6d39fde6d17205
Dmytro Samodurov <dimasamodurov@gmail.com>
2017-03-17 14:50:08 +0200

    Extract Inventory logic to a class.

--- ec2hosts.py
+++ ec2hosts.py
@@ -1,50 +1,60 @@
-import json 
+#!/usr/bin/env python
+
+import json
 import subprocess
 import operator
 import argparse
 
 
-def main():
-    args = parse_args()
+class Ec2Hosts(object):
+    def aws_instances_description(self):
+        if not hasattr(self, 'instances'):
+            self.instances = json.loads(self.aws_describe_instances())
+        return self.instances
 
-    if args.list:
-        host_groups = get_ec2_hosts()
-        print_ec2_hosts(host_groups)
+    def aws_describe_instances(self):
+        return subprocess.check_output(["aws", "ec2", "describe-instances"])
+
+    def get_ec2_hosts(self):
+        def get_instances(reservation): return reservation['Instances']
+
+        def get_dns_name(instance): return instance['PublicDnsName']
+
+        groups = self.aws_instances_description()
+
+        instances = reduce(operator.add, map(get_instances, groups['Reservations']))
+
+        names = map(get_dns_name, instances)
+
+        return names
+
+    def get_ec2_metadata(self):
+        meta = {
+            "all": self.get_ec2_hosts(),
+            "_meta": self.aws_instances_description()
+        }
+        return meta
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Preprocess ec2 described instances.')
+    parser = argparse.ArgumentParser(description='Dynamic Inventory retrieving description of EC2 instances.')
     parser.add_argument('--list', action='store_true', help='list all hosts')
     args = parser.parse_args()
-    if not (args.list):
-        raise Exception('--list argument must be specified.')
+    if not args.list:
+        parser.print_help()
     return args
-
-
-def aws_describe_instances():
-    return subprocess.check_output(["aws", "ec2", "describe-instances"])
-
-
-def get_ec2_hosts():
-    def get_instances(reservation): return reservation['Instances']
-
-    def get_dns_name(instance): return instance['PublicDnsName']
-
-    instances_description = aws_describe_instances()
-
-    groups = json.loads(instances_description)
-
-    instances = reduce(operator.add, map(get_instances, groups['Reservations']))
-
-    names = map(get_dns_name, instances)
-
-    return names
 
 
 def print_ec2_hosts(host_groups):
     print json.dumps(host_groups, indent=2, sort_keys=True)
 
 
+def main():
+    args = parse_args()
+
+    if args.list:
+        print_ec2_hosts(Ec2Hosts().get_ec2_metadata())
+
+
 if __name__ == '__main__':
     main()
-
