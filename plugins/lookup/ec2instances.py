4469a4b74ec7cd95fb0cef40b45b94836526d73e
Dmytro Samodurov <dimasamodurov@gmail.com>
2017-04-19 14:31:12 +0300

    Add filtering ability to the ec2instances plugin.

--- plugins/lookup/ec2instances.py
+++ plugins/lookup/ec2instances.py
@@ -7,17 +7,28 @@ try:
     from __main__ import display
 except ImportError:
     from ansible.utils.display import Display
+
     display = Display()
 
-# Plugin filters out EC2 instances based on tags match criteria
-# {{ lookup('ec2instances', {'tags': ['test1']}) }}.
-class LookupModule(LookupBase):
 
+# Plugin filters out EC2 instances based on tags match criteria
+# {{ lookup('ec2instances', {'Project': 'cms'}) }}.
+class LookupModule(LookupBase):
     def run(self, terms, variables=None, **kwargs):
         meta = self.inventory_meta(variables["inventory_file"])
-        #import pdb; pdb.set_trace()
-        return []
+        hostvars = meta['_meta']['hostvars']
 
+        matched_hosts = [k for k, v in hostvars.iteritems() if self.tags_matched(terms, v['Tags'])]
+
+        return matched_hosts
+
+    def tags_matched(self, terms, tags):
+        for term in terms:
+            for name, value in term.iteritems():
+                for tag in tags:
+                    if tag['Key'] == name and tag['Value'] == value:
+                        return True
+        return False
 
     def inventory_meta(self, inventory_file):
         return json.loads(subprocess.check_output([inventory_file, "--list"]))
